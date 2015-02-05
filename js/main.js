80bc5097b5859a0ff7a3931dac8b7a6ad90d8d4e
David <davichay@gmail.com>
2015-02-04 22:26:13 -0600

    propuesta terminada
    esta es la version propuesta para empezar hacer el theme en wordpress

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ $(function(){
 		$(".menu-principal").toggle(100);
 
 	});
+	$('.search-top').on("click",function(event){
+		$("#show-search-box").toggle(190);
+	});
 	$( window ).resize(function() {
 		if($(document).width()>767){
 			$(".menu-principal").removeAttr("style");
 			$('.show-menu').removeClass("show-menu-on");
+			$("#show-search-box").removeAttr("style");
 		}
  	 	
 	});
@@ -22,5 +26,29 @@ $(function(){
 			$('.menu-principal').removeClass("menu-flotante");
 		}
 	});
+
+
+  	$("#owl-slider").owlCarousel({
+ 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+ 	  autoPlay: 3000
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  	});
+  	var owl = $("#owl-slider");
+  	// Custom Navigation Events
+	  $(".next").click(function(){
+	    owl.trigger('owl.next');
+	  })
+	  $(".prev").click(function(){
+	    owl.trigger('owl.prev');
+	  })
 
 });
